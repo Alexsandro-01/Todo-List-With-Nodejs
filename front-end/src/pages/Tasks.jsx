7d360433442c23d1bb9523bcb8f00e855ae6f4f2
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-06-19 11:24:22 -0300

    feat: criação da pagina de tasks e seu layout básico

--- front-end/src/pages/Tasks.jsx
+++ front-end/src/pages/Tasks.jsx
@@ -1,8 +1,71 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../styles/tasks.css';
+
+
 function Tasks() {
+  const [user, setUser] = useState({});
+  const navigate = useNavigate();
+  const logedUser = localStorage.getItem('taskUser');
+
+  function isLoged() {
+    if (!logedUser) {
+      navigate('/login');
+    }
+    else {
+      const userObj = JSON.parse(logedUser);
+      setUser(userObj);
+    }
+  }
+
+  useEffect(() => {
+    isLoged();
+  }, []);
+  const { name } = user;
   return (
-    <main>
-      tasks page.
-    </main>
+    <div className='tasks-page'>
+      <header>
+        <h2>Task Force</h2>
+        <p>
+          Olá <strong>{ name }</strong>
+        </p>
+      </header>
+      <main>
+        <div className='new-task'>
+          <input type="text" placeholder='new task' />
+          <button>+</button>
+        </div>
+        <section>
+          <ul>
+            <li>
+              <input type="checkbox" id="1" />
+              <label htmlFor="1">
+                Tarefa 1
+              </label>
+            </li>
+            <li>
+              <input type="checkbox" id="2" />
+              <label htmlFor="2">
+                Tarefa 1
+              </label>
+            </li>
+            <li>
+              <input type="checkbox" id="3" />
+              <label htmlFor="3">
+                Tarefa 1
+              </label>
+            </li>
+            <li>
+              <input type="checkbox" id="4" />
+              <label htmlFor="4">
+                Tarefa 1
+              </label>
+            </li>
+          </ul>
+        </section>
+      </main>
+    </div>
+      
   )
 }
 
